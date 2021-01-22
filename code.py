d74e4540952d02a4c99526b7145dc8534dd83c19
Javid Husseynly <javidhusseynly@gmail.com>
2021-01-22 18:57:34 -0500

    Added new implementation

--- code.py
+++ code.py
@@ -1,12 +1,34 @@
 def are_valid_groups(students, groups):
+        
+        newstudents = []
+        for student in students:
+                newstudents.append(str(student))
+        students = newstudents
+
+        newgroups = []
+        for group in groups:
+                newgroup = []
+                for student in group:
+                        newgroup.append(str(student))
+                newgroups.append(newgroup)
+        groups = newgroups
+
+
+        for group in groups:
+                if len(group) < 2 or len(group) > 3:
+                        return False
+
+        usedstudents = []
         for group in groups:
                 for student in group:
-                        if student in students:
+                        if student in usedstudents:
+                                return False
+                        elif student not in students:
+                                return False
+                        elif student in students:
                                 students.remove(student)
+                                usedstudents.append(student)
         if not students:
                 return True
         else:
                 return False
-
-#rip daniels code
-#rip rupens code
