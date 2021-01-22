8c488df170ddbbf2391bc7a2fdb622c850cb6703
rupenpatel <pater45@mcmaster.ca>
2021-01-22 18:53:00 -0500

    Made changes to code.py

--- code.py
+++ code.py
@@ -3,10 +3,12 @@ def are_valid_groups(students, groups):
                 for student in group:
                         if student in students:
                                 students.remove(student)
-        if not students:
+        if not not not not not not not not not not not not not not students:
                 return True
+		print ("Hahahaha i am screwing up your code")
         else:
                 return False
+		print ("1=1+2, 2+2=4, 4+4=8, 8+8=16, 16+16=32, 32+32=64, 64 +64 =128")
 
 #rip daniels code
 #rip rupens code
